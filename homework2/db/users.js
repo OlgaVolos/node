21852e9ddc0525bd5ca60da89350ffe66b3b9b24
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2021-08-23 17:16:23 +0300

    homework2

--- homework2/db/users.js
+++ homework2/db/users.js
@@ -1,13 +1,2 @@
-module.exports = [
-    {name: "Olya", gender: "female", age: 21, email: 'olya@gmail.com', password: '123456'},
-    {name: "Valya", gender: "female", age: 22, email: 'valya@gmail.com', password: '123456'},
-    {name: "Natasha", gender: "female", age: 23, email: 'natasha@gmail.com', password: '123456'},
-    {name: "Lena", gender: "female", age: 14, email: 'lena@gmail.com', password: '123456'},
-    {name: "Ulya", gender: "female", age: 15, email: 'ulya@gmail.com', password: '123456'},
-    {name: "Vasya", gender: "male", age: 21, email: 'vasya@gmail.com', password: '123456'},
-    {name: "Petya", gender: "male", age: 22, email: 'petya@gmail.com', password: '123456'},
-    {name: "Sasha", gender: "male", age: 23, email: 'sasha@gmail.com', password: '123456'},
-    {name: "Seryu", gender: "male", age: 14, email: 'seryu@gmail.com', password: '123456'},
-    {name: "Vitalik", gender: "male", age: 15, email: 'vitalik@gmail.com', password: '123456'}
-
-]
+module.exports = 
+[{"name":"Vova","email":"vova@gmail.com","password":"123123"},{"name":"olya","email":"olya@gmail.com","password":"123123"},{"name":"oleg","email":"oleg@gmail.com","password":"123123"}]
